6fe7dc9acd8c7b3c8d8832d84850eb7b62184752
arone415 <105476658+arone415@users.noreply.github.com>
2025-09-24 12:16:34 -0700

    I learned about sout because of Hoang! Awesome...

---
+++ workbook1/workbook1d/StringApplication/src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main(String[] args) {
+
+        String item = "apples";
+        int quantity = 3;
+        double price = 7.10;
+
+        System.out.println("You bought " + quantity + " " + item + " for $"
+        + price);
+    }
+}
--- workbook1/workbook1d/ex-cats/src/main/java/com/pluralsight/Main.java
+++ workbook1/workbook1d/ex-cats/src/main/java/com/pluralsight/Main.java
@@ -4,6 +4,15 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("sout is dope");
+        boolean imHappy = true;
+        boolean hadBreakfast = true;
+
+        if (imHappy && hadBreakfast) {
+            System.out.println("Nice job!");
+        } else if (imHappy && !hadBreakfast) {
+            System.out.println("you could be hungry");
+        } else if (!imHappy && !hadBreakfast) {
+            System.out.println("Do something about it");
+        }
     }
 }
