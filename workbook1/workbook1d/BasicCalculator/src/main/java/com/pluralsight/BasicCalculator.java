73e88902ea5569099451e8cf60d0fee84f000e2d
arone415 <105476658+arone415@users.noreply.github.com>
2025-09-25 14:05:56 -0700

    added BasicCalculator project

---
+++ workbook1/workbook1d/BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
@@ -0,0 +1,46 @@
+package com.pluralsight;
+import java.util.*;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        float num1 = scanner.nextFloat();
+
+        System.out.println("Enter the second number: ");
+        float num2 = scanner.nextFloat();
+
+        System.out.println("Possible calculations:");
+        System.out.println(" (A)dd");
+        System.out.println(" (S)ubtract");
+        System.out.println(" (M)ultiply");
+        System.out.println(" (D)ivide");
+        System.out.println("Please select an option: 'A' 'S' 'M' 'D' ");
+        char choice = scanner.next().toUpperCase().charAt(0);
+
+        float result = 0; //placeholder
+
+        switch(choice) {
+            case 'A':
+                result = num1 + num2;
+                System.out.printf("Result: %.2f + %.2f = %.2f%n", num1, num2, result);
+                break;
+            case'S':
+                result = num1 - num2;
+                System.out.printf("Result: %.2f - %.2f = %.2f%n", num1, num2, result);
+                break;
+            case 'M':
+                result = num1 * num2;
+                System.out.printf("Result: %.2f * %.2f = %.2f%n", num1, num2, result);
+                break;
+            case 'D':
+                result = num1 / num2;
+                System.out.printf("Result: %.2f / %.2f = %.2f%n", num1, num2, result);
+                break;
+            default:
+                System.out.println("Invalid option. Please select 'A' 'S' 'M' 'D' ");
+        }
+
+    }
+}
