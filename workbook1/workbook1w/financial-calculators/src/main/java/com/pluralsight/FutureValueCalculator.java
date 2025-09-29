d0ab41923a101ddaffb771abb37b055619bb4b3d
arone415 <105476658+arone415@users.noreply.github.com>
2025-09-29 11:38:12 -0700

    added FutureValueCalculator for workshop 1w

--- workbook1/workbook1w/financial-calculators/src/main/java/com/pluralsight/FutureValueCalculator.java
+++ workbook1/workbook1w/financial-calculators/src/main/java/com/pluralsight/FutureValueCalculator.java
@@ -6,4 +6,22 @@ public class FutureValueCalculator {
         double futureValue = deposit * Math.pow(1+ (annualRate/365), 365 * years);
         return futureValue;
     }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your deposit: ");
+        int deposit = input.nextInt();
+
+        System.out.println("enter your interest rate");
+        double annualRate = input.nextDouble();
+
+        System.out.println("how many years?");
+        int years = input.nextInt();
+
+        annualRate *= 0.01;
+        double futureValue = calculateFutureValue(deposit,annualRate,years);
+
+        System.out.printf("Here is your compound interest for your item: $%.2f", futureValue);
+    }
 }
