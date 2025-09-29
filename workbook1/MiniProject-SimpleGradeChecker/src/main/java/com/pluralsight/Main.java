08e94bf71590060a70f1786b3cc16bcea64497f2
arone415 <105476658+arone415@users.noreply.github.com>
2025-09-29 09:47:36 -0700

    added week 1 recap mini project

---
+++ workbook1/MiniProject-SimpleGradeChecker/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your exam score: ");
+        int score = input.nextInt();
+
+        if (score >= 90 && score <= 100) {
+            System.out.println("A");
+        } else if (score >= 80 && score <= 89) {
+            System.out.println("B");
+        } else if (score >= 70 && score <= 79) {
+            System.out.println("C");
+        } else if (score >= 60 && score <= 69) {
+            System.out.println("D");
+        } else if (score < 60) {
+            System.out.println("F");
+        } else {
+            System.out.println("enter valid score");
+        }
+    }
+}
