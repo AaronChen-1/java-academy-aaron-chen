21011229c69633489475cd283bdae1a40d3b2618
arone415 <105476658+arone415@users.noreply.github.com>
2025-09-29 13:37:37 -0700

    started PresentValueCalculator.java for workshop 1w

---
+++ workbook1/workbook1w/financial-calculators/src/main/java/com/pluralsight/PresentValueCalculator.java
@@ -0,0 +1,14 @@
+/* package com.pluralsight;
+
+public class PresentValueCalculator {
+    public static double calculatePresentValue(double monthlyPayOut, int annualRate, int years) {
+        //p * [(1-(1+4)^-n)/r]
+        double presentValue =
+        return monthlyPayOut;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+ */
