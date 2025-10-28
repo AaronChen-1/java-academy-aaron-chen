f7ea85cf7c435d96d332462ea0f153f21f766a65
arone415 <105476658+arone415@users.noreply.github.com>
2025-10-27 21:32:01 -0700

    Phase 4 -- Program Entry Point Class 5: Program Package: com.dealership

--- workshops/CarDealershipApp/src/main/java/com/dealership/Program.java
+++ workshops/CarDealershipApp/src/main/java/com/dealership/Program.java
@@ -1,4 +1,10 @@
 package com.dealership;
 
+import com.dealership.ui.UserInterface;
+
 public class Program {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.display();
+    }
 }
